69a40dd5379669c42d6fc43c2196c449ae6dd5c7
Guillermo Rafael <guillermo@athenaalliance.com>
2025-07-17 19:02:01 -0700

    Cambio de ruta

--- src/paginas/PaginadeHome.tsx
+++ src/paginas/PaginadeHome.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Navegacion from "../componentes/Navegacion";
-import "../public/styles/index.css";
+import "../../public/styles/index.css";
 
 const Wrapper = styled.div`
     width: 100vw;
